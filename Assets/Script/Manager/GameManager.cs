e1b35884de71bb405e98bb91f9d5a0a2b5d9efc5
NgDinhThi <dinhthit25@gmail.com>
2025-04-17 16:28:29 +0700

    sửa giao diện Unity

--- Assets/Script/Manager/GameManager.cs
+++ Assets/Script/Manager/GameManager.cs
@@ -1,15 +1,16 @@
-using UnityEngine;
+﻿using UnityEngine;
 using System;
 
 public class GameManager : MonoBehaviour
 {
-    [SerializeField] private Player player;
+    [SerializeField] private Player player; // Tham chiếu đến đối tượng Player trong game
 
     private void Update()
     {
-        if(Input.GetKeyDown(KeyCode.R))
+        // Kiểm tra nếu người chơi nhấn phím R
+        if (Input.GetKeyDown(KeyCode.R))
         {
-            player.ResetPlayer();
-        }    
+            player.ResetPlayer(); // Gọi hàm ResetPlayer để reset trạng thái nhân vật
+        }
     }
 }
