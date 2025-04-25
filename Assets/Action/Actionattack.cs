35c21543cfdca696a69c13febb2f8ea758589ce0
NgDinhThi <dinhthit25@gmail.com>
2025-04-25 23:57:47 +0700

    quái có thể gây st cho người chơi

---
+++ Assets/Action/Actionattack.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+
+public class Actionattack : FSMaction
+{
+    [Header("Config")]
+    [SerializeField] private float Dmg;
+    [SerializeField] private float TimeDmg;
+
+
+
+    private EnemyBrain enemy;
+    private float timer;
+
+
+    private void Awake()
+    {
+        enemy = GetComponent<EnemyBrain>();
+    }
+
+    public override void Act()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    private void AtkPlayer()
+    {
+        if (enemy.nguoichoi == null)
+            return;
+
+        timer -= Time.time;
+        if (timer <=0f)
+        {
+            IdamageAble player = enemy.nguoichoi.GetComponent<IdamageAble>();
+            player.TakeDamage(Dmg);
+            timer = TimeDmg;
+        }
+    }
+}
