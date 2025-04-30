990ae7c80cf5571b8bb71b74215696e43bb4fa4c
NgDinhThi <dinhthit25@gmail.com>
2025-04-30 16:14:02 +0700

    tạo hiệu ứng đòn đánh

---
+++ Assets/Script/Projectiles/Projectiles.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class Projectiles : MonoBehaviour
+{
+    [Header("Config")]
+    [SerializeField] private float speed;
+
+    public Vector3 huongbay {  get; set; }
+
+    private void Update()
+    {
+        transform.Translate(huongbay *(speed * Time.deltaTime));
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("va chạm nè");
+    }
+}
