75809cae7511da6f239426e3d2280df75a2b3169
NgDinhThi <dinhthit25@gmail.com>
2025-04-30 20:24:26 +0700

    chế tạo vũ khí

---
+++ Assets/Script/Vũ khí/Weapon.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System;
+
+
+public enum LoaiVK
+{
+    Phep,
+    CanChien
+}
+
+
+[CreateAssetMenu(fileName ="Vukhi_")]
+/*
+chỉ dùng được với class kế thừa từ ScriptableObject
+ phải nằm dưới CreateAssetMenu
+ */
+
+public class Weapon : ScriptableObject
+{
+    [Header("Config")]
+
+    public Sprite icon;
+    public LoaiVK loaiVK;
+    public float dmg;
+
+    public Projectiles projectilesPrefab;
+
+    public float luongMana;
+}
