4ca7ae3a15857539c6bd45a17090ed730ba43c80
NgDinhThi <dinhthit25@gmail.com>
2025-05-07 21:01:16 +0700

    tạo danh mục vũ khí để  trong inventory

---
+++ Assets/Script/Inventory/ItemWeapon.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+
+[CreateAssetMenu(menuName ="Items/Weapon ", fileName ="ItemWeapon")]
+public class ItemWeapon : InventoryItems
+{
+    [Header("Weapon")]
+
+    public Weapon weapon;
+}
