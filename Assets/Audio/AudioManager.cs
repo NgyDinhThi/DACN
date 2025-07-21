c9fe8e1e5531958639488338738d55c807d9a0b5
NgDinhThi <dinhthit25@gmail.com>
2025-07-21 18:35:26 +0700

    hoàn thiện âm l;ượng hơn
    mục tiêu cần làm:
    kết nối với phần volume để điều chỉnh âm lượng
    fix bug còn lại

--- Assets/Audio/AudioManager.cs
+++ Assets/Audio/AudioManager.cs
@@ -1,4 +1,5 @@
 using System;
+using Unity.VisualScripting;
 using Unity.VisualScripting.Antlr3.Runtime.Tree;
 using UnityEngine;
 using UnityEngine.Audio;
@@ -6,13 +7,25 @@ public class AudioManager : MonoBehaviour
 {
    public Sound[] sounds;
 
+   public static AudioManager instance;
+   
     private void Awake()
     {
+        if (instance == null) instance = this;
+        else
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+
+
+        DontDestroyOnLoad(gameObject);
         foreach (Sound s in sounds)
         {
             s.source = gameObject.AddComponent<AudioSource>();
             s.source.clip = s.clip;
-
+            s.source.loop = s.loop;
             s.source.volume = s.volume;
             s.source.pitch = s.pitch;
         }
@@ -27,7 +40,8 @@ public class AudioManager : MonoBehaviour
     public void Play(string name)
     {
        Sound s = Array.Find(sounds,sound => sound.name == name);
-       s.source.Play();
+        if (s == null) return;
+        s.source.Play();
 
     }    
 }
