b846318677f45768bd4073f637973bf6483e363b
NgDinhThi <dinhthit25@gmail.com>
2025-07-23 21:45:22 +0700

    tạo after credit

---
+++ Assets/menu setting/CreditScript.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+public class CreditScript : MonoBehaviour
+{
+    [Header("Setting")]
+    [SerializeField] private float ScrollSpeed;
+    [SerializeField] private float endY;
+    [SerializeField] private CreditRole role; // enum ở đây
+
+    private RectTransform rectTransform;
+    private bool hasNotified = false;
+
+    private void Start()
+    {
+        rectTransform = GetComponent<RectTransform>();
+    }
+
+    private void Update()
+    {
+        rectTransform.anchoredPosition += new Vector2(0, ScrollSpeed * Time.deltaTime);
+
+        if (!hasNotified && rectTransform.anchoredPosition.y >= endY)
+        {
+            hasNotified = true;
+
+            // Gọi hàm theo enum
+            if (role == CreditRole.TitleText)
+                EndingSceneManager.Instance.NotifyTitleFinished();
+            else if (role == CreditRole.CreditText)
+                EndingSceneManager.Instance.NotifyCreditFinished();
+        }
+    }
+}
---
+++ Assets/menu setting/EndingSceneManager.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+
+public class EndingSceneManager : MonoBehaviour
+{
+    public static EndingSceneManager Instance;
+
+    [SerializeField] private GameObject announceMenu;
+
+    private bool isTitleDone = false;
+    private bool isCreditDone = false;
+
+    private void Awake()
+    {
+        if (Instance == null) Instance = this;
+        else Destroy(gameObject);
+    }
+
+    // Gọi từ TitleText
+    public void NotifyTitleFinished()
+    {
+        isTitleDone = true;
+        CheckFinish();
+    }
+
+    // Gọi từ CreditText
+    public void NotifyCreditFinished()
+    {
+        isCreditDone = true;
+        CheckFinish();
+    }
+
+    // Kiểm tra nếu cả hai đã xong
+    private void CheckFinish()
+    {
+        if (isTitleDone && isCreditDone)
+        {
+            Debug.Log("✅ Title & Credit đã chạy xong! Hiện announceMenu");
+            announceMenu.SetActive(true);
+        }
+    }
+}
